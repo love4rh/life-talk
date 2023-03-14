b7a7ef699321319b660226152575d3e371580140
Myunghyun Kim <love4rh@paran.com>
2023-03-14 22:29:34 +0900

    do little things

--- webApp/src/view/TalkEditor.js
+++ webApp/src/view/TalkEditor.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import * as T from './StyledElements';
 
-import { isvalid } from '../common/common';
 import { Dark as TM } from '../common/theme';
 
 import { Header } from './Header';
@@ -13,9 +12,11 @@ import CheckIcon from '@mui/icons-material/Check';
 
 import { actGoBackToList } from './actions';
 
+import TextField from '@mui/material/TextField';
+
 
 const EditBody = styled.div`
-  background-color: ${TM.listPanelColor};
+  padding: 10px;
 `;
 
 
@@ -31,10 +32,11 @@ class TalkEditor extends React.Component {
   constructor (props) {
     super(props);
 
-    const { cw } = this.props;
+    const { cw, board } = this.props;
 
     this.state = {
-      cw 
+      cw,
+      title: board.title
     };
   }
 
@@ -54,30 +56,42 @@ class TalkEditor extends React.Component {
     actGoBackToList();
   }
 
+  handleChangeTitle = (ev) => {
+    const me = ev.target;
+
+    this.setState({ title: me.value });
+  }
+
   render () {
-    const { cw } = this.state;
+    const { cw, title } = this.state;
 
-    return (
-      <div>
-        <Header
-          ch={TM.headerHeight}
-          cw={cw}
-          color={TM.titleColor}
-          bgColor={TM.listPanelColor}
-          borderColor={TM.bgScreen}
-          center={true}
-          headButton={{ onClick: actGoBackToList, element: <ArrowBackIcon size="medium" /> }}
-          tailButton={{ onClick: this.handleConfirm, element: <CheckIcon size="medium" /> }}
-        >
-          {'Edit Talk'}
-        </Header>
+    return (<>
+      <Header
+        ch={TM.headerHeight}
+        cw={cw}
+        color={TM.titleColor}
+        bgColor={TM.listPanelColor}
+        borderColor={TM.bgScreen}
+        center={true}
+        headButton={{ onClick: actGoBackToList, element: <ArrowBackIcon size="medium" /> }}
+        tailButton={{ onClick: this.handleConfirm, element: <CheckIcon size="medium" /> }}
+      >
+        {'Edit Talk'}
+      </Header>
 
-        <T.Container bgColor={TM.listPanelColor}>
-          Implementing...
-        </T.Container>
-
-      </div>
-    );
+      <T.Container bgColor={TM.listPanelColor}>
+        <EditBody>
+          <TextField
+            required
+            fullWidth
+            label="Talk Title"
+            value={title}
+            variant="standard"
+            onChange={this.handleChangeTitle}
+          />
+        </EditBody>
+      </T.Container>
+    </>);
   }
 
 };
